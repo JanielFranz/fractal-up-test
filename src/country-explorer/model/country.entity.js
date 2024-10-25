fd822bd09ff7ea7a94a5c3c24ca477d245eb962a
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-25 11:35:51 -0500

    feat(model): flag image url added to country entity

--- src/country-explorer/model/country.entity.js
+++ src/country-explorer/model/country.entity.js
@@ -1,12 +1,12 @@
 export class Country {
     constructor({ name, capital, currency, languages = [],
-                    continent, countryImgUrl, continentImgUrl }) {
+                    continent, countryImgUrl, countryFlagImgUrl }) {
         this.name = name;
         this.capital = capital;
         this.currency = currency;
         this.languages = languages;
         this.continent = continent;
         this.countryImgUrl = countryImgUrl;
-        this.continentImgUrl = continentImgUrl;
+        this.countryFlagImgUrl = countryFlagImgUrl;
     }
 }
