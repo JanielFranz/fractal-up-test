3fac626a92c55a3ffed4c92e35fcc66b2f36eba8
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-25 11:37:53 -0500

    feat(component): country card item logic added to show the dialog

--- src/country-explorer/components/country-card-item.component.vue
+++ src/country-explorer/components/country-card-item.component.vue
@@ -1,29 +1,46 @@
 <script>
 import {Country} from "../model/country.entity.js";
+import CountryItemDialog from "./country-item-dialog.component.vue";
 
 export default {
   name: "country-card-item",
+  components: {CountryItemDialog},
   props: {
     country: {
       type: Country,
       required: true
+    }
+  },
+  data() {
+    return {
+      dialogVisible: false
+    }
+  },
+  methods: {
+    openDialog() {
+      this.dialogVisible = true;
     }
   }
 }
 </script>
 
 <template>
-  <pv-card style="width: 22rem; height:22rem; overflow: hidden" class="mb-4">
-    <template #header>
-      <img :src="country.countryImgUrl" alt="country image" style="width:22rem; height: 16rem;">
-    </template>
-    <template #title>
-      <h3 class="title-center">{{ country.name }} </h3>
-    </template>
-    <template #subtitle>
-      <h4 class="subtitle-center">{{ country.continent }}</h4>
-    </template>
-  </pv-card>
+  <div class="card" @click="openDialog">
+    <pv-card style="width: 22rem; height:22rem; overflow: hidden" class="mb-4">
+      <template #header>
+        <img :src="country.countryImgUrl" alt="country image" style="width:22rem; height: 16rem;">
+      </template>
+      <template #title>
+        <h3 class="title-center">{{ country.name }} </h3>
+      </template>
+      <template #subtitle>
+        <h4 class="subtitle-center">{{ country.continent }}</h4>
+      </template>
+    </pv-card>
+  </div>
+
+  <country-item-dialog :country="country" v-bind:visible="dialogVisible"/>
+
 </template>
 
 <style scoped>
