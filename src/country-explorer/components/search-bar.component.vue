c63322e279e56288b6e759ef8381f13fb3b1d38e
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-25 17:17:44 -0500

    feat(component): search bar style with grid

--- src/country-explorer/components/search-bar.component.vue
+++ src/country-explorer/components/search-bar.component.vue
@@ -1,15 +1,51 @@
 <script>
 
+
 export default {
   name: "search-bar",
+  props: {
+    continents: {
+      type: Array,
+      required: true
+    }
+  },
   data() {
     return {
-      countrySearched: ""
+      selectedContinent: null,
+      countrySearched: "",
+      dialogVisible: false,
+      popOverVisible: false
     }
   },
   methods: {
+    //#region Event Handlers
     onWordAdded() {
       this.$emit("word-added", this.countrySearched);
+    },
+    onSearchBarClicked() {
+      console.log("search bar clicked");
+      this.dialogVisible=true;
+    },
+    //#endregion
+
+    openDialog() {
+      this.dialogVisible = true;
+    },
+
+    closeDialog() {
+      this.dialogVisible = false;
+    },
+
+    togglePopOver() {
+      this.popOverVisible = !this.popOverVisible;
+    },
+
+    toggle(event) {
+      this.$refs.op.toggle(event);
+    },
+    selectContinent(continent) {
+      this.selectedContinent = continent;
+      this.$refs.op.hide();
     }
   }
 }
@@ -20,6 +56,7 @@ export default {
     <template #center>
       <pv-icon-field>
         <pv-input-text
+            @click="toggle"
             placeholder="Pais Escribe el pais que deseas ver"
             v-model="countrySearched"
             @input="onWordAdded"
@@ -30,6 +67,20 @@ export default {
       </pv-icon-field>
     </template>
   </pv-toolbar>
+  <pv-popover ref="op">
+    <h2>Filtrar por continentes</h2>
+    <div class="grid mt-4">
+      <div
+          v-for="continent in continents"
+          :key="continent.name"
+          class="col-10 sm:col-6 lg:col-4"
+      >
+        <img :src="continent.continentImgUrl" alt="continent.name" style="height: 5rem; width: 6rem;"/>
+        <p>{{ continent.name }}</p>
+      </div>
+    </div>
+
+  </pv-popover>
 </template>
 
 <style scoped>
