19d37abed551b76f6032581c1752777bb0c551b2
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-25 00:48:41 -0500

    chore(component): styles added to country card-item

--- src/country-explorer/components/country-card-item.component.vue
+++ src/country-explorer/components/country-card-item.component.vue
@@ -13,15 +13,28 @@ export default {
 </script>
 
 <template>
-  <pv-card style="width: 25rem; overflow: hidden">
+  <pv-card style="width: 22rem; height:22rem; overflow: hidden" class="mb-4">
     <template #header>
-      <img :src="country.countryImgUrl" alt="country image">
+      <img :src="country.countryImgUrl" alt="country image" style="width:22rem; height: 16rem;">
     </template>
-    <template #title>{{ country.name }}</template>
-    <template #subtitle>{{ country.continent }}</template>
+    <template #title>
+      <h3 class="title-center">{{ country.name }} </h3>
+    </template>
+    <template #subtitle>
+      <h4 class="subtitle-center">{{ country.continent }}</h4>
+    </template>
   </pv-card>
 </template>
 
 <style scoped>
-
+  .title-center{
+    text-align: center;
+    margin-top: 0.1rem;
+    font-size: 1rem;
+  }
+  .subtitle-center {
+    text-align: center;
+    margin-top: 0.1rem;
+    font-size: 1rem;
+  }
 </style>
