b2a1b4d44c9248c087074bfa5788a00c9850a186
JanielFranz <102563480+JanielFranz@users.noreply.github.com>
2024-10-25 11:36:40 -0500

    feat(component): dialog component added

---
+++ src/country-explorer/components/country-item-dialog.component.vue
@@ -0,0 +1,54 @@
+<script>
+
+
+import {Country} from "../model/country.entity.js";
+
+export default {
+  name: "country-item-dialog",
+  props: {
+    country: {
+      type: Country,
+      required: true
+    },
+    visible: {
+      type: Boolean,
+      required: true
+    }
+  },
+  methods: {
+    onCloseSelected() {
+      this.$emit('close-selected', false);
+    }
+  }
+}
+</script>
+
+<template>
+  <pv-dialog :visible="visible" @hide="onCloseSelected">
+    <div class="flex items-center gap-4 mb-4">
+      <img :src="country.countryImgUrl" :alt="country.name"/>
+    </div>
+    <div class="flex items-center gap-4 mb-4">
+      <p>
+        <img :src="country.countryFlagImgUrl"
+             :alt="country.countryFlagImgUrl"
+             class="inline-block h-6 w-6 mr-2"/>
+        <span>{{ country.name }}</span>
+        <span>{{ country.continent}}</span>
+      </p>
+    </div>
+    <div class="flex items-center gap-4 mb-2">
+      <p>Capital: {{country.capital}}</p>
+      <p>Language:
+        <span v-for="(language, index) in country.languages" :key="index">
+          {{ language }} <span v-if="index < country.languages.length -1">, </span>
+        </span>
+      </p>
+      <p>Currency: {{country.currency}}</p>
+    </div>
+  </pv-dialog>
+</template>
+
+<style scoped>
+
+</style>
